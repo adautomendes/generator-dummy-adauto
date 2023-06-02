4c9085699f2863043af2734024544426b673fb36
Adauto Mendes Bernardes Júnior <adauto.mendes.ext@ericsson.com>
2023-06-02 11:45:02 -0300

    First draft

---
+++ generators/app/index.js
@@ -0,0 +1,25 @@
+'use strict';
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+    constructor(args, opts) {
+        super(args, opts);
+        this.option('str1', { type: String });
+        this.option('bool1', { type: Boolean });
+    }
+
+    initializing() {
+        this.log('*** APP ***');
+
+        this.composeWith(require.resolve('../sub-gen1'));
+        this.composeWith(require.resolve('../sub-gen2'));
+
+        this.log(`str1 => ${this.options['str1']}`);
+        this.log(`str2 => ${this.options['str2']}`);
+        this.log(`str3 => ${this.options['str3']}`);
+
+        this.log(`bool1 => ${this.options['bool1']}`);
+        this.log(`bool2 => ${this.options['bool2']}`);
+        this.log(`bool3 => ${this.options['bool3']}`);
+    }
+};
---
+++ generators/sub-gen2/index.js
@@ -0,0 +1,22 @@
+'use strict';
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+    constructor(args, opts) {
+        super(args, opts);
+        this.option('opt3', { type: String });
+        this.option('bool3', { type: Boolean });
+    }
+
+    initializing() {
+        this.log('*** SUB-GEN2 ***');
+
+        this.log(`str1 => ${this.options['str1']}`);
+        this.log(`str2 => ${this.options['str2']}`);
+        this.log(`str3 => ${this.options['str3']}`);
+
+        this.log(`bool1 => ${this.options['bool1']}`);
+        this.log(`bool2 => ${this.options['bool2']}`);
+        this.log(`bool3 => ${this.options['bool3']}`);
+    }
+};
